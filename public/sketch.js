7f59821b8498ea327c8b3fb2a707c49086709bcf
afasano <afasano925@gmail.com>
2017-05-22 20:43:23 -0400

    Corrected the mousepress and added touch screen support

--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,7 @@
 var socket;
 function setup(){
-  createCanvas(1920,947);
+  //1920,947
+  createCanvas(windowWidth,windowHeight);
   background(51);
 
   socket=io.connect('https://collaboradoodle.herokuapp.com/');
@@ -12,6 +13,7 @@ function newDrawing(data){
   fill(255,0,100);
   ellipse(data.x, data.y, 36, 36);
 }
+/*
 function mouseMoved(){
   console.log('Sending: '+mouseX+','+mouseY);
   if(mouseIsPressed){
@@ -25,11 +27,47 @@ function mouseMoved(){
     ellipse(mouseX,mouseY,36,36);
   }
 }
+*/
 function draw(){
 //  console.log(mouseX+','+mouseY);
 
 }
-function mouseclicked(){
+/*
+function mouseClicked(){
+  console.log('Sending: '+mouseX+','+mouseY);
+  var data={
+    x:mouseX,
+    y:mouseY
+  }
+  socket.emit('mouse',data);
+  noStroke();
+  fill(255);
+  ellipse(mouseX,mouseY,36,36);
+}
+*/
+function mousePressed(){
+  console.log('Sending: '+mouseX+','+mouseY);
+  var data={
+    x:mouseX,
+    y:mouseY
+  }
+  socket.emit('mouse',data);
+  noStroke();
+  fill(255);
+  ellipse(mouseX,mouseY,36,36);
+}
+function touchStarted(){
+  console.log('Sending: '+mouseX+','+mouseY);
+  var data={
+    x:mouseX,
+    y:mouseY
+  }
+  socket.emit('mouse',data);
+  noStroke();
+  fill(255);
+  ellipse(mouseX,mouseY,36,36);
+}
+function touchMoved(){
   console.log('Sending: '+mouseX+','+mouseY);
   var data={
     x:mouseX,
