c3e6be0c0e0756d67b279f28fadf2352c09522b2
afasano <afasano925@gmail.com>
2017-05-22 20:10:40 -0400

    Changed canvas and drawing

--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,6 @@
 var socket;
 function setup(){
-  createCanvas(600,400);
+  createCanvas(1920,947);
   background(51);
 
   socket=io.connect('https://collaboradoodle.herokuapp.com/');
@@ -14,6 +14,23 @@ function newDrawing(data){
 }
 function mouseMoved(){
   console.log('Sending: '+mouseX+','+mouseY);
+  if(mouseIsPressed){
+    var data={
+      x:mouseX,
+      y:mouseY
+    }
+    socket.emit('mouse',data);
+    noStroke();
+    fill(255);
+    ellipse(mouseX,mouseY,36,36);
+  }
+}
+function draw(){
+//  console.log(mouseX+','+mouseY);
+
+}
+function mouseclicked(){
+  console.log('Sending: '+mouseX+','+mouseY);
   var data={
     x:mouseX,
     y:mouseY
@@ -22,8 +39,4 @@ function mouseMoved(){
   noStroke();
   fill(255);
   ellipse(mouseX,mouseY,36,36);
-}
-function draw(){
-//  console.log(mouseX+','+mouseY);
-
 }
