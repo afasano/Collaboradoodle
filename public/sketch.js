4f04782ea8bdf407ccfcd223a118749f6bb61dfe
afasano <afasano925@gmail.com>
2017-05-22 20:55:09 -0400

    Fixed mouse press

--- public/sketch.js
+++ public/sketch.js
@@ -13,7 +13,7 @@ function newDrawing(data){
   fill(255,0,100);
   ellipse(data.x, data.y, 36, 36);
 }
-/*
+
 function mouseMoved(){
   console.log('Sending: '+mouseX+','+mouseY);
   if(mouseIsPressed){
@@ -27,12 +27,12 @@ function mouseMoved(){
     ellipse(mouseX,mouseY,36,36);
   }
 }
-*/
+
 function draw(){
 //  console.log(mouseX+','+mouseY);
 
 }
-/*
+
 function mouseClicked(){
   console.log('Sending: '+mouseX+','+mouseY);
   var data={
@@ -44,18 +44,8 @@ function mouseClicked(){
   fill(255);
   ellipse(mouseX,mouseY,36,36);
 }
-*/
-function mousePressed(){
-  console.log('Sending: '+mouseX+','+mouseY);
-  var data={
-    x:mouseX,
-    y:mouseY
-  }
-  socket.emit('mouse',data);
-  noStroke();
-  fill(255);
-  ellipse(mouseX,mouseY,36,36);
-}
+
+
 function touchStarted(){
   console.log('Sending: '+mouseX+','+mouseY);
   var data={
